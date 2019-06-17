7abc69b9ed1bcd1cc4e566bf161c5896a6d259b5
Artur Kerge <artur@kerge.eu>
2019-06-17 16:23:27 +0300

    Quicker .bashrc updating

---
+++ update-bashrc.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -   
+#title          :update-bashrc.sh
+#description    :Teensy script to update .bashrc
+#author         :Artur Kerge
+#date           :2019-06-17
+#version        :0.1    
+#usage          :./update-bashrc.sh
+#notes          :       
+#bash_version   :4.4.12(1)-release
+#============================================================================
+
+rm .bashrc
+wget https://github.com/akerge/shell/raw/master/.bashrc
