8f9adee83a2a2867a062c0d01502d80192fb64f5
Artur Kerge <artur@kerge.eu>
2019-06-20 10:35:45 +0300

    Add clarifying echoes

--- patch-bashrc.sh
+++ patch-bashrc.sh
@@ -7,6 +7,8 @@
 #bash_version   :4.4.12(1)-release
 #============================================================================
 
+echo "Pulling updates"
 git pull
+echo "Diff'ing .bashrc"
 diff -u ~/.bashrc .bashrc > bashrc.patch
 patch ~/.bashrc bashrc.patch
