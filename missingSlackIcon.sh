9e41b6d695496bc3073af838a24998fbdf63c836
Artur Kerge <artur@kerge.eu>
2019-06-30 22:16:50 +0300

    Add check for running as sudo

--- missingSlackIcon.sh
+++ missingSlackIcon.sh
@@ -6,4 +6,11 @@
 #version         :0.1
 #usage		 :./missingSlashIcon.sh
 #==============================================================================
-sudo echo "StartupWMClass=Slack" >> /var/lib/snapd/desktop/applications/slack_slack.desktop
+if (( $EUID != 0 )); then
+    echo "Please run as sudo"
+    exit
+else
+    sudo echo "StartupWMClass=Slack" >> /var/lib/snapd/desktop/applications/slack_slack.desktop
+    echo "After Slack restart icon should return to default."
+    echo "Have a nice day!"
+fi
