b8797548c6d159493473b758ff16b3b72b9d9066
Artur Kerge <artur@kerge.eu>
2019-07-01 17:23:58 +0300

    Remove newline from read

--- install-applications.sh
+++ install-applications.sh
@@ -4,7 +4,7 @@ printf "This script will update apt and install the following:\n"
 printf "\napt\n===\n neofetch\n snap\n git\n"
 printf "\nsnap\n====\n slack\n spotify\n atom\n"
 printf "\n\n"
-read -t 12 -p "\nPress Ctrl-C to cancel or do nothing for 12s to continue...\n"
+read -t 12 -p "Press Ctrl-C to cancel or do nothing for 12s to continue..."
 sudo apt update && sudo apt upgrade
 sudo apt install neofetch snapd
 sudo snap install slack --classic
