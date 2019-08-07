766ed38a9b5a63baa03748f34d1fbb43c454fa2d
Artur Kerge <artur@kerge.eu>
2019-08-07 22:26:06 +0300

    Change wget to copy instead

--- backup-and-replace-bashrc.sh
+++ backup-and-replace-bashrc.sh
@@ -3,11 +3,12 @@
 #description    :Teensy script to update .bashrc
 #author         :Artur Kerge
 #date           :2019-06-17
-#version        :0.1    
-#usage          :./update-bashrc.sh
+#version        :0.2
+#usage          :./backup-and-replace-bashrc.sh
 #notes          :       
 #bash_version   :4.4.12(1)-release
 #============================================================================
 
-rm ~/.bashrc
-wget --output-document ~/.bashrc https://github.com/akerge/shell/raw/master/.bashrc
+mv ~/.bashrc ~/.bashrc.original
+cp .bashrc ~/.bashrc
+echo "Done."
