0406be326bc2f52a2486a75d3c04d034b1ffc24d
Artur Kerge <artur@kerge.eu>
2019-09-26 15:09:25 +0300

    Move app listing to another variable and array, add function and confirmation instead of wait time

--- install-applications.sh
+++ install-applications.sh
@@ -1,17 +1,43 @@
 #!/bin/bash
 
-printf "This script will update apt and install the following:\n"
-printf "\napt\n===\n neofetch\n snap\n git\n"
-printf "\nsnap\n====\n slack\n spotify\n atom\n"
-printf "\n\n"
-read -t 12 -p "Press Ctrl-C to cancel or do nothing for 12s to continue..."
-sudo apt update && sudo apt upgrade
-sudo apt install neofetch snapd texlive pdflatex texstudio python3 python3-pip 
-sudo snap install slack --classic
-sudo snap install spotify
-sudo snap install atom --classic
-printf "\nDone!\n"
+apps="neofetch snapd texlive texstudio python3 python3-pip vim" 
+snaps=("spotify" "atom --classic" "slack --classic")
+dash="======"
 
+printf "This script will update and upgrade apt and install the following:\n\n"
+printf "apt:\n$dash\n"
+printf "$apps\n\n"
+printf "snaps:\n$dash\n"
+
+state="echo"
+snapit="sudo snap install "
+function floop() {
+  for ((i=0; i<${#snaps[@]}; i++));
+    do
+      eval "$1 ${snaps[i]}"
+    done
+}
+
+floop "$state"
+
+printf "\nContinue? [Y/n]"
+read RESPONSE
+case $RESPONSE in
+  [nN])
+    exit
+    ;;
+  *) 
+    printf "\n\nUpdating and upgrading...\n\n"
+    sudo apt update && sudo apt upgrade
+    sudo apt install $apps 
+    floop "$snapit"
+    printf "\nDone!\n"
+    ;;
+esac
+#TODO
+# implement exit codes and print message regarding if successful or not
+# check if snapd exists
+#
 # Varia
 ## speedreading
 #   https://github.com/xypiie/spread0r flashes text
