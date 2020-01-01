c9267c91270de73f96641cd1a419251d60d8087e
Artur Kerge <artur@kerge.eu>
2020-01-01 13:40:32 +0200

    Add print function

--- selectToInstall.sh
+++ selectToInstall.sh
@@ -3,3 +3,18 @@
 ######################################################
 # Script install all or select applications to install
 ######################################################
+
+# Array of applications to be installed via apt
+apts=("neofetch" "glances")
+
+# print function for easier reading
+function printElement(){
+	printf "Install $1? [Y/n] "
+}
+
+for ((a=0; a<"${#apts[@]}"; a++));
+        do
+                printElement ${apts[a]}
+                printf "\n"
+        done
+
